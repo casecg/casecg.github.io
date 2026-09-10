58a5c2395286c2e413afc63a7fe7ac3597b4a975
Vortex <hg.garrett@icloud.com>
2026-09-10 18:21:59 -0400

    Delete aero directory

--- aero/games.js
+++
@@ -1,4 +0,0 @@
-// Add your own HTML games here. Paths are relative to index.html.
-// Example (leave commented until you add the real files):
-// { title: 'My game', description: 'A short description.', path: 'games/my-game/index.html' }
-window.AERO_GAMES = [];
