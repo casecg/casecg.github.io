f96cb16a584314fc37a9b18a3defd0e8c18165ca
Vortex <hg.garrett@icloud.com>
2026-09-10 21:34:56 -0400

    Update games.js

--- games.js
+++ games.js
@@ -1,4 +1,10 @@
 // Add your own HTML games here. Paths are relative to index.html.
 // Example (leave commented until you add the real files):
 // { title: 'My game', description: 'A short description.', path: 'games/my-game/index.html' }
-window.AERO_GAMES = [];
+window.AERO_GAMES = [
+  {
+    title: 'GUST Browser',
+    description: 'Proxy',
+    path: 'games/my-game/index.html'
+  }
+];
